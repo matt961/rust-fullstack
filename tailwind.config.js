868a7d67b71bf61c5b82e5608e31684f6a153b70
matt961 <matt961@protonmail.ch>
2024-01-15 23:02:14 -0800

    idk

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    container: {
+      center: true
+    }
+  },
+  plugins: [
+    require("@tailwindcss/forms")
+  ],
+}
